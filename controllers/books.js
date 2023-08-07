77eb5c107066ccefdf6c999fe33ab374c2db013e
Laura Frances <laura742@hotmail.fr>
2023-08-07 08:29:23 +0200

    add comments

--- controllers/books.js
+++ controllers/books.js
@@ -1,41 +1,47 @@
 const Book = require('../models/book');
-const fs = require('fs');
+const fs = require('fs'); // module permettant d'interagir avec le système de fichiers local
 
+// créer un livre
 exports.createBook = (req, res) => {
-    const bookObject = JSON.parse(req.body.book);
-    delete bookObject._id;
-    delete bookObject._userId;
-    // const rating = bookObject.rating;
-    const book = new Book({
-        ...bookObject,
-        userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    const bookObject = JSON.parse(req.body.book); // on stocke les données du corps de la requête
+    // delete bookObject._id;
+    // delete bookObject._userId;
+
+    const book = new Book({ // crée un nouvel objet Book, défini par le model Book
+        ...bookObject, // on ajoute les propriétés suivantes à l'objet book :
+        userId: req.auth.userId, // attribution de l'id de l'user authentifié à la propriété userId, pour lier le livre à l'user
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`, // on ajoute une URL au livre qui est stocké dans le file /images
     });
 
-    book.save()
+    book.save() // on enregistre le nouvel objet book dans la data base
         .then(() => { res.status(201).json({ message: 'Livre enregistré !' }) })
-        .catch(() => { res.status(400).json({ message: 'Veuillez remplir tous les champs.'}) })
+        .catch(() => { res.status(400).json({ message: 'Erreur lors de la sauvegarde du livre.'}) })
 };
 
+// récupérer un livre 
 exports.getOneBook = (req, res) => {
     Book.findOne({ _id: req.params.id })
         .then(book => res.status(200).json(book))
         .catch(error => res.status(404).json({ error }));
 }
 
+// modifier un livre
 exports.modifyBook = (req, res) => {
-    const bookObject = req.file ? {
-        ...JSON.parse(req.body.book),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body };
+  // on stocke les données à mettre à  jour pour le livre existant :
 
-    delete bookObject.userId;
-    Book.findOne({ _id: req.params.id })
-        .then((book) => {
-            if (book.userId != req.auth.userId) {
+    const bookObject = req.file ? { // si un fichier est présent dans la requête alors...
+        ...JSON.parse(req.body.book), // ...on récupère les données à partir du corps de la requête et on étend ces données...
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}` //...avec cette propriété
+    } : { ...req.body }; // si pas de fichier, on envoie seulement le texte des propriétés du livre
+
+    delete bookObject.userId; // on s'assure que l'ID de l'user associé au livre ne sera pas modifié lors de la màj
+
+    Book.findOne({ _id: req.params.id }) // on recherche le livre dans la database
+        .then((book) => { 
+            if (book.userId != req.auth.userId) { // on vérifie l'id de l'user associé au livre et l'id de l'user authentifié
                 res.status(401).json({ message: 'Non-autorisé' });
             } else {
-                Book.updateOne({ _id: req.params.id }, 
+                Book.updateOne({ _id: req.params.id }, // on met à jour le livre dans la database en utilisant son id
                   { ...bookObject, _id: req.params.id })
                     .then(() => res.status(200).json({ message: 'Livre modifié!' }))
                     .catch(error => res.status(401).json({ error }));
@@ -46,6 +52,7 @@ exports.modifyBook = (req, res) => {
         });
 };
 
+// notation d'un livre
 exports.rateBook = (req, res) => {
 
     const bookId = req.params.id; // on extrait l'id du livre
@@ -98,6 +105,7 @@ exports.rateBook = (req, res) => {
       });
   };
 
+// tri des 3 meilleurs livres
 exports.getBestBooks = (req, res, next) => {
     Book.find()
       .sort({ averageRating: -1 }) // Trie par ordre décroissant de la propriété `averageRating`
@@ -110,16 +118,17 @@ exports.getBestBooks = (req, res, next) => {
       });
   };
 
+// suppression d'un livre
 exports.deleteBook = (req, res) => {
     Book.findOne({ _id: req.params.id }) // on recherche un livre spécifique dans la base de données
         .then(book => { // on exécute le livre trouvé
-            if (book.userId != req.auth.userId) { // on vérifie l'auth de l'user 
+            if (book.userId != req.auth.userId) { // on vérifie si l'user auth a le même id que l'user du livre
                 res.status(401).json({ message: 'Not authorized' });
             } else {
                 const filename = book.imageUrl.split('/images/')[1];
                 fs.unlink(`images/${filename}`, () => { // on supprime l'image associée au livre
-                    Book.deleteOne({ _id: req.params.id }) // onsupprime le livre de la DB 
-                        .then(() => { res.status(200).json({ message: 'Objet supprimé !' }) })
+                    Book.deleteOne({ _id: req.params.id }) // on supprime le livre de la DB 
+                        .then(() => { res.status(200).json({ message: 'Livre supprimé !' }) })
                         .catch(error => res.status(401).json({ error }));
                 });
             }
@@ -129,6 +138,7 @@ exports.deleteBook = (req, res) => {
         });
 };
 
+// récupérer tous les livres (home)
 exports.getAllBooks = (req, res, next) => {
     Book.find()
         .then(books => res.status(200).json(books))
--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,10 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt'); // bibliothèque bcrypt pour le cryptage du mdp
+const jwt = require('jsonwebtoken'); // bibliothèque pour générer des jetons d'auth
 
 const User = require('../models/user');
-require('dotenv').config();
+require('dotenv').config(); // bibliothèque pour charger les variables d'environnement (identifiants de mongoDB)
 
-
+// enregistrement d'un utilisateur
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10) // cryptage du mdp
         .then(hash => {
@@ -19,18 +19,19 @@ exports.signup = (req, res, next) => {
         .catch(error => res.status(500).json({ error }));
 };
 
+// connexion de l'utilisateur
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
                 return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
             }
-            bcrypt.compare(req.body.password, user.password)
+            bcrypt.compare(req.body.password, user.password) // on compare le mdp de la requête et le mdp de la database 
                 .then(valid => {
                     if (!valid) {
                         return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
                     }
-                    res.status(200).json({
+                    res.status(200).json({ // si les mdp concordent, alors on renvoie un objet json avec l'id et le token
                         userId: user._id,
                         token: jwt.sign(
                             { userId: user._id },
