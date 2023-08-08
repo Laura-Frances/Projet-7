00df554532975af89f3f11bc70dd33adbfee949a
Laura Frances <laura742@hotmail.fr>
2023-08-08 13:55:22 +0200

    last commit

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express'); // importation du module express pour notre app
 const mongoose = require('mongoose'); // importation du module mongoose pour l'interaction avec mongoDB
-
+const app = express();
 const bookRoutes = require('./routes/bookRoutes');
 const userRoutes = require('./routes/user');
 const path = require('path');
-const cors = require('cors');
 
 // Connect to mongoDB
 const dotenv = require("dotenv")
@@ -15,19 +14,16 @@ mongoose.connect(connectionString)
   .then(() => { console.log('Connecté à MongoDB'); })
   .catch((error) => { console.error('Erreur de connexion à MongoDB', error); });
 
-  const app = express();
-  app.use(cors());
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader("Content-Security-Policy", "default-src 'self'");
   next();
 });
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 
 // on importe nos Routes
 app.use('/api/books', bookRoutes);
