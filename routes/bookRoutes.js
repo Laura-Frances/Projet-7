83fea66ee379ff0afae7e9593d083ddfd47cf2ff
Laura Frances <laura742@hotmail.fr>
2023-08-08 13:52:21 +0200

    adding fixs middlewares inside routes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const auth = require('../middleware/auth')// on passe l'argument auth dans nos routes à protéger
-const multer = require('../middleware/multer-config')
-
+const auth = require('../middleware/auth');// on passe l'argument auth dans nos routes à protéger
+const multerSharp = require('../middleware/multer-sharp');
 const bookCtrl = require('../controllers/books');
 
+// définition des routes spécifiques 
 router.get('/', bookCtrl.getAllBooks);
 router.get('/bestrating', bookCtrl.getBestBooks); 
 router.get('/:id', bookCtrl.getOneBook);
-router.post('/', auth, multer, bookCtrl.createBook);
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.post('/', auth, multerSharp.upload, multerSharp.optimizeImage, bookCtrl.createBook);
+router.put('/:id', auth, multerSharp.upload, multerSharp.optimizeImage, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.post('/:id/rating', auth, bookCtrl.rateBook);
 
 module.exports = router;
+
